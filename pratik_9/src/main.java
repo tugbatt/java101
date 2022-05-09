139cbeee1b1cd93d40c3cc902821ce02bf0917d3
tt <tugbatrn@hotmail.com>
2022-05-09 15:12:40 +0300

    t

--- pratik_9/src/main.java
+++ pratik_9/src/main.java
@@ -7,15 +7,15 @@ public class main {
         Scanner input = new Scanner(System.in);
         System.out.println("Hava sıcaklık değerini giriniz: ");
         x = input.nextInt();
-        if (x<5) {
+        if (x < 5) {
             System.out.println("Kayak yapabilirsin.");
         } else if (x >= 5 && x <= 15) {
             System.out.println("Sinemaya gidebilirsin.");
-        }else if (x > 15 && x <= 25) {
+        } else if (x > 15 && x <= 25) {
             System.out.println("Piknik yapabilirsin.");
-        }else if (x>25){
-        System.out.println("Yüzmeye gidebilirsin.");
+        } else if (x > 25) {
+            System.out.println("Yüzmeye gidebilirsin.");
+        }
     }
-}
 }
 
